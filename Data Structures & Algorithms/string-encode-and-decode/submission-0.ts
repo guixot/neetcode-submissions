34258b22c8fbde0f23e009689e51728792479ca8
Paco Guixot <fguixotsoriano@gmail.com>
2026-08-27 17:34:11 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let result = ""
+
+        strs.forEach(str => {
+            result += str.length + "#" + str
+        });
+
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const result: string[] = [];
+
+        let p = 0;
+
+        let currentStringCharCount = "";
+
+        while (p < str.length) {
+            let currentChar = str[p];
+
+            if (currentChar === "#") {
+                result.push(str.slice(p + 1, p + Number(currentStringCharCount) + 1));
+                p += 1 + Number(currentStringCharCount);
+                currentStringCharCount = "";
+            } else {
+                currentStringCharCount += currentChar;
+                p++;
+            }
+        }
+
+        return result;
+    }
+}
