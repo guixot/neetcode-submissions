2323a2731da273c0397edb9fe9729c58672ab322
Paco Guixot <fguixotsoriano@gmail.com>
2026-08-28 18:38:32 +0200

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums: number[]): number {
+        let candidate = nums[0];
+        let count = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] === candidate) {
+                count++;
+            } else {
+                count--;
+                if (count === 0) {
+                    candidate = nums[i];
+                    count++;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
