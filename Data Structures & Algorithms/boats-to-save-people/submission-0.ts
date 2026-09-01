d1a462b50db3d7a341f5f9b4122ee6764a209ab9
Paco Guixot <fguixotsoriano@gmail.com>
2026-09-01 09:57:31 +0200

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people: number[], limit: number): number {
+        people.sort((a, b) => b - a);
+
+        let left = 0;
+        let right = people.length - 1;
+
+        let result = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                left++;
+            }
+            result++;
+        }
+
+        return result;
+    }
+}
